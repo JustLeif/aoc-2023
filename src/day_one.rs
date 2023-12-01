0ddf01a90508dfb847855c25c9beef9d3ef99ed1
leif <leif@elliottnet.com>
2023-12-01 13:44:45 -0500

    solution to part one day one.

--- src/day_one.rs
+++ src/day_one.rs
@@ -19,11 +19,13 @@ pub fn part_one(input: &str) -> usize {
 
 #[cfg(test)]
 mod test {
-    use crate::day_one::part_one;
+    use crate::{day_one::part_one, inputs::day_one_part_one_prompt};
 
     #[test]
     fn test_part_one() {
         let input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
         assert_eq!(part_one(input), 142);
+        let res = part_one(day_one_part_one_prompt());
+        println!("{res}");
     }
 }

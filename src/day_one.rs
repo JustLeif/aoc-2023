3dbedc2f6b7cb3eaee65bbf5fa751a6d17ffde60
leif <leif@elliottnet.com>
2023-12-01 13:36:51 -0500

    part one of day one

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,3 +1,29 @@
-pub fn solution() -> bool {
-    true
+pub fn part_one(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let chars: Vec<char> = line.chars().filter(|c| c.is_numeric()).collect();
+            let mut res = chars
+                .iter()
+                .enumerate()
+                .filter(|(idx, _)| idx == &0 || idx == &(chars.len() - 1))
+                .map(|(_, val)| *val)
+                .collect::<String>();
+            if res.len() == 1 {
+                res.push(res.chars().nth(0).unwrap());
+            }
+            res.parse::<usize>().unwrap()
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::day_one::part_one;
+
+    #[test]
+    fn test_part_one() {
+        let input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
+        assert_eq!(part_one(input), 142);
+    }
 }
